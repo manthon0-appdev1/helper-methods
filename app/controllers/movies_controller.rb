158b0863b3e491e1f7317e461e8659311cf1430f
manthon0 <manthon0@chicagobooth.edu>
2023-01-09 23:26:08 +0000

    new movie form now nests

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,9 +4,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    matching_movies = Movie.all
-
-    @list_of_movies = matching_movies.order({ :created_at => :desc })
+    @list_of_movies = Movie.order( created_at: :desc )
 
     respond_to do |format|
       format.json do
@@ -18,17 +16,15 @@ class MoviesController < ApplicationController
   end
 
   def show
-    the_id = params.fetch(:id)
-
-    matching_movies = Movie.where({ :id => the_id })
-
-    @the_movie = matching_movies.first
+    #@the_movie = Movie.where(id: params.fetch(:id)).first
+    #@the_movie = Movie.find_by(id: params.fetch(:id)))
+    @the_movie = Movie.find(params.fetch(:id))
   end
 
   def create
     @the_movie = Movie.new
-    @the_movie.title = params.fetch("query_title")
-    @the_movie.description = params.fetch("query_description")
+    @the_movie.title = params.fetch(:movie).fetch(:title)
+    @the_movie.description = params.fetch(:movie).fetch(:description)
 
     if @the_movie.valid?
       @the_movie.save
@@ -39,16 +35,11 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    the_id = params.fetch(:id)
-
-    matching_movies = Movie.where({ :id => the_id })
-
-    @the_movie = matching_movies.first
+    @the_movie = Movie.find(params.fetch(:id))
   end
 
   def update
-    the_id = params.fetch(:id)
-    the_movie = Movie.where({ :id => the_id }).first
+    @the_movie = Movie.find(params.fetch(:id))
 
     the_movie.title = params.fetch("query_title")
     the_movie.description = params.fetch("query_description")
@@ -62,8 +53,7 @@ class MoviesController < ApplicationController
   end
 
   def destroy
-    the_id = params.fetch(:id)
-    the_movie = Movie.where({ :id => the_id }).first
+    @the_movie = Movie.find(params.fetch(:id))
 
     the_movie.destroy
 
